d40eea1127a591a7fc10600607f5f1295460c129
Marcelo Buergo <mbuergo@frba.utn.edu.ar>
2023-05-31 19:33:27 -0300

    TP 01 - Calcular pi - Cambios y presentacion (READMEs)

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -2,21 +2,16 @@
 #include <iomanip>
 using namespace std;
 
-long double liebniz(int& n, int piRef){ 
+long double liebniz(int& n){ 
     long double resu = 0;
-    int comp = 0;
-    while (comp != piRef){
+    while (resu < 3.141592 || resu > 3.141593){ // Negando (resu > 3.141592 && resu < 3.141593) obtengo (resu < 3.141592 || resu > 3.141593) mediante leyes logicas.
         resu = (n%2==0) ? resu + 4.0/(2*n+1) : resu - 4.0/(2*n+1); // Originalmente es (-1) elevado a n, pero hacer (-1)*(-1) un billón de veces tomaría horas, el condicional reduce todo eso a ver si n es par o impar para que 1 sea o no negativo.
-        comp = resu * 1e6; // Corro la coma seis lugares y me quedo con la parte entera para poder comparar los 7 digitos con piRef.
         n++;
     }
     return resu;
 }
-main(){ 
+int main(){ 
     int n = 0; // n de Iteraciones
-    float pi = 3.141592; // Pi modificable
-    int piRef = pi*1e6; // Pi vuelto a entero con siete digitos listo para comparar
-    cout << "Sumando hasta llegar a pi (precision 7): " << setprecision(7) << pi;
-    cout << "\n\nAlcanzado el primer valor equivalente con el siguiente resultado (precision 20): " << setprecision(20) << liebniz(n,piRef) << "...\n\nIteraciones cometidas: "<< n << endl;
+    cout << "Sumando hasta llegar a Pi: 3.141592"  << "\n\nAlcanzado el primer valor equivalente con el siguiente resultado: " << setprecision(20) << liebniz(n) << "...\n\nIteraciones: "<< n;
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# AyED
+# Repositorio para la materia Algoritmos y Estructura de Datos
+Marcelo Nahuel Buergo
+Curso: K1043
